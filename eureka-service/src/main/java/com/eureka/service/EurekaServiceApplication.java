d73bf69645d4dcf15a6c6cf3d3f993d9f4fbdaf9
gdelucca <gdelucca45@gmail.com>
2024-11-20 16:32:38 -0300

    Corrije config-data para Eureka

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -1,6 +1,6 @@
 package com.eureka.service;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
